290f74ad7559af4cf28e7f7e18229b40b1252a1c
Clark <amorimclark@gmail.com>
2021-12-29 09:33:12 -0300

    fixing some errors again

--- components/Results.jsx
+++ components/Results.jsx
@@ -2,8 +2,7 @@ import Thumbnail from "./Thumbnail"
 
 function Results({results}){
   return (
-    <div className="my-10 sm:grid md:grid-cols-2 xl:grid-cols-3
-    2xl:grid grid-cols-4">
+    <div className="my-10 sm:grid md:grid-cols-2 xl:grid-cols-3">
       {results.map((result) => 
         <Thumbnail key={result.id} result={result} />
       )}
